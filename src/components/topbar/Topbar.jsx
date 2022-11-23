722a07dfd986ddd35cfbcf67c6fd502b962d9ea0
IBUNHABIBU <ibunhabib@gmail.com>
2022-11-23 13:29:44 +0300

    Add humberger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ const Topbar = () => (
   <div className="topbar">
     <div className="wrapper">
       <div className="left">
-        <a href="#intro" className="logo">geneous.</a>
+        <a href="#intro" className="logo">Hobbiest.</a>
         <div className="itemContainer">
           <Person className="icon" />
           <span>+255717722477</span>
@@ -16,7 +16,13 @@ const Topbar = () => (
           <span>ibunhabib@gmail.com</span>
         </div>
       </div>
-      <div className="right" />
+      <div className="right">
+        <div className="humburger">
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </div>
     </div>
   </div>
 );
