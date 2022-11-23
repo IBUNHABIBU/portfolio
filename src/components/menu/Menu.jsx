7494a1edf1d713389d05eb2d9b21be45e1b24b63
IBUNHABIBU <ibunhabib@gmail.com>
2022-11-23 16:48:03 +0300

    Add menu component

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Menu = () => (
+  <div className="menu">
+    <ul>
+      <li>
+        <a href="#intro">
+          intro
+        </a>
+        <a href="#portfolio">
+          portfolio
+        </a>
+        <a href="#works">
+          works
+        </a>
+        <a href="#testimonials">
+          testimonials
+        </a>
+        <a href="#contacts">contacts</a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Menu;
