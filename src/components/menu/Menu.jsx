72e229d04b0ac5cce8c6317933239c187a7c3898
IBUNHABIBU <ibunhabib@gmail.com>
2022-11-24 05:38:21 +0300

    Add sidebar click event

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,40 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './menu.scss';
+import MenuItems from '../menuItems/MenuItems';
+import menuItems from '../../constants';
 
 const Menu = ({ menuOpen, setMenuOpen }) => (
   <div className={`menu ${menuOpen && 'active'}`}>
     <ul>
-      <li>
-        <a href="#intro" onClick={() => setMenuOpen(false)}>
-          intro
-        </a>
-      </li>
-      <li>
-        <a href="#portfolio">
-          portfolio
-        </a>
-      </li>
-      <li>
-        <a href="#works">
-          works
-        </a>
-      </li>
-      <li>
-        <a href="#testimonials">
-          testimonials
-        </a>
-      </li>
-      <li>
-        <a href="#contacts">contacts</a>
-      </li>
+      {
+        menuItems.map(({ item, id }) => <MenuItems key={id} id={id} item={name} setMenuOpen={setMenuOpen} />)
+      }
     </ul>
   </div>
 );
 
 Menu.prototype = {
-    menuOpen: PropTypes.bool.isRequired,
-    setMenuOpen: PropTypes.func.isRequired,
-}
+  menuOpen: PropTypes.bool.isRequired,
+  setMenuOpen: PropTypes.func.isRequired,
+};
 
 export default Menu;
