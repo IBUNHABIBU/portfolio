be718bd15d2d744f1c51564abb788d04f272565d
IBUNHABIBU <ibunhabib@gmail.com>
2022-11-25 08:23:18 +0300

    Add menu items

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './menu.scss';
+import { HelpOutline } from '@material-ui/icons';
 import MenuItems from '../menuItems/MenuItems';
 import menuItems from '../../constants';
 
@@ -8,7 +9,7 @@ const Menu = ({ menuOpen, setMenuOpen }) => (
   <div className={`menu ${menuOpen && 'active'}`}>
     <ul>
       {
-        menuItems.map(({ item, id }) => <MenuItems key={id} id={id} item={name} setMenuOpen={setMenuOpen} />)
+        menuItems.map(({ name, id }) => <MenuItems key={id} id={id} item={name} setMenuOpen={setMenuOpen} />)
       }
     </ul>
   </div>
--- src/components/menuItems/MenuItems.jsx
+++ src/components/menuItems/MenuItems.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MenuItems = ({ item, id }) => (
+const MenuItems = ({ item, id, setMenuOpen }) => (
   <li>
-    {/* <a href={id} onClick={() => setMenuOpen(false)}>{item}</a> */}
-    <a href="#porr" onClick={() => setMenuOpen(false)}>hello</a>
+    <a href={id} onClick={() => setMenuOpen(false)}>{item}</a>
+    
+    {/* <div>
+      <p>{item}</p>
+      <p>{id}</p>
+    </div> */}
   </li>
 
 );
