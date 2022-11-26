50571f37d1294c722365097b71fb0af32a83cbd8
IBUNHABIBU <ibunhabib@gmail.com>
2022-11-26 20:17:42 +0300

    Add portfolio head

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './portfolio.scss';
 
 const Portfolio = () => (
-  <div className="portfolio" id="portfolio">Portfolio</div>
+  <div className="portfolio" id="portfolio">
+    <h1>Portfolio</h1>
+    <ul>
+      <li>Featured</li>
+      <li>Ruby on rails</li>
+      <li>React</li>
+      <li>Javascript</li>
+      <li>HTML & CSS</li>
+    </ul>
+  </div>
 );
 
 export default Portfolio;
