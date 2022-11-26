fa497892cea3bf44f7e9c4bed66579a774694064
IBUNHABIBU <ibunhabib@gmail.com>
2022-11-26 20:55:03 +0300

    Style the portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,15 +5,37 @@ const Portfolio = () => (
   <div className="portfolio" id="portfolio">
     <h1>Portfolio</h1>
     <ul>
-      <li>Featured</li>
+      <li className="active">Featured</li>
       <li>Ruby on rails</li>
       <li>React</li>
       <li>Javascript</li>
       <li>HTML & CSS</li>
     </ul>
+    
     <div className="container">
       <div className="item">
         <img src="carRental.png" alt="car rental" />
+        <h3>Car rental App</h3>
+      </div>
+      <div className="item">
+        <img src="carRental.png" alt="car rental" />
+        <h3>Car rental App</h3>
+      </div>
+      <div className="item">
+        <img src="carRental.png" alt="car rental" />
+        <h3>Car rental App</h3>
+      </div>
+      <div className="item">
+        <img src="carRental.png" alt="car rental" />
+        <h3>Car rental App</h3>
+      </div>
+      <div className="item">
+        <img src="carRental.png" alt="car rental" />
+        <h3>Car rental App</h3>
+      </div>
+      <div className="item">
+        <img src="carRental.png" alt="car rental" />
+        <h3>Car rental App</h3>
       </div>
     </div>
   </div>
