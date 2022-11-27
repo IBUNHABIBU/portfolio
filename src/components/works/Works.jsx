f842b61552dc2a779638ce534a0d7c636961f1df
IBUNHABIBU <ibunhabib@gmail.com>
2022-11-27 22:46:16 +0300

    Start work file

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -7,8 +7,26 @@ function Works() {
       <div className="slider">
         <div className="container">
           <div className="item">
-            <div className="left">left</div>
-            <div className="right">Right</div>
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="mobile.png" alt="mobile" />
+                </div>
+                <h2>title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aperiam libero voluptatum suscipit sapiente quidem aspernatur velit voluptates
+                  laudantium sint, est ad ipsa tenetur iure iusto ipsam odio placeat quasi voluptas.
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+              <img
+                src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2020/09/remote_work_infographic_blog.png?auto=format&q=60&w=450&h=270&fit=crop&crop=faces"
+                alt="websitetemplate"
+              />
+            </div>
           </div>
         </div>
       </div>
