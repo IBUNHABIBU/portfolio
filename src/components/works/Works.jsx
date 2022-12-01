076f7c1f91046db0e80c240bb16f6f5fc4905a1a
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-01 20:50:15 +0300

    Fix the lint errors

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,15 +3,16 @@ import { data } from '../../constants';
 import './works.scss';
 
 function Works() {
-  const [slide, setSlide] = useState(0)
+  const [slide, setSlide] = useState(0);
 
   const handleClick = (way) => {
-    way === "left" ? setSlide(slide > 0 ? slide-1 : 2) : setSlide(slide < data.length - 1 ? slide + 1 : 0)
-  }
-      
+    way === 'left' ? setSlide(slide > 0 ? slide - 1 : 2) : setSlide(slide < data.length - 1 ? slide + 1 : 0);
+  };
+
   return (
     <div className="works" id="works">
-      <div className="slider"
+      <div
+        className="slider"
         style={{ transform: `translateX(-${slide * 100}vw)` }}
       >
         { data.map((item) => {
@@ -46,8 +47,8 @@ function Works() {
           );
         })}
       </div>
-      <img src="arrow.png" alt="arrow left" className="arrow left" onClick={() => handleClick("left")}/>
-      <img src="arrow.png" alt="arrow right" className="arrow right"onClick={() => handleClick()} />
+      <img src="arrow.png" alt="arrow left" aria-hidden="true" className="arrow left" onClick={() => handleClick('left')} />
+      <img src="arrow.png" alt="arrow right" aria-hidden="true" className="arrow right" onClick={() => handleClick()} />
     </div>
   );
 }
