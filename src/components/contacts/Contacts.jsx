13976ee32c7addd49eff2d5ba0fa223a0b040010
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-04 17:42:18 +0300

    Add contact form

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './contacts.scss';
 
 const Contacts = () => (
-  <div className="contact" id="contacts">Contacts</div>
+  <div className="contact" id="contacts">
+    <div className="left">
+      <img src="/shake.svg" alt="" />
+    </div>
+    <div className="right">
+      <h2>Contact.</h2>
+      <form>
+        <input type="text" placeholder='Email' />
+        <textarea placeholder='Message' />
+        <button type='submit'>Send</button>
+      </form>
+    </div>
+  </div>
 );
 
 export default Contacts;
