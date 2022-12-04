b84c29dac94772ae30b64b8c1e8013733d2f8c23
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-04 16:18:33 +0300

    Add testimonialData

--- src/components/testmonials/Testimonials.jsx
+++ src/components/testmonials/Testimonials.jsx
@@ -8,23 +8,25 @@ const Testimonials = () => (
     <div className="container">
       {
         testimonialData.map((data) => {
-          const { id, name, title, img, icon, desc } = data;
-          return(
-      <div className="card">
-        <div className="top">
-          <img src="assets/right-arrow.png" alt="" className="left" />
-          <img src={img} alt="" className="user" />
-          <img src={icon} alt="" className="right" />
-        </div>
-        <div className="center">
-          {desc}
-        </div>
-        <div className="bottom">
-          <h3>{name}</h3>
-          <h4>{title}</h4>
-        </div>
-      </div>
-         )
+          const {
+            id, name, title, img, icon, desc,
+          } = data;
+          return (
+            <div className="card" key={id}>
+              <div className="top">
+                <img src="assets/right-arrow.png" alt="" className="left" />
+                <img src={img} alt="" className="user" />
+                <img src={icon} alt="" className="right" />
+              </div>
+              <div className="center">
+                {desc}
+              </div>
+              <div className="bottom">
+                <h3>{name}</h3>
+                <h4>{title}</h4>
+              </div>
+            </div>
+          );
         })
       }
     </div>
