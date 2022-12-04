f6ee979c24486de6a6b7d5daaef5f3a4457f008b
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-04 18:52:49 +0300

    Style the form

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './contacts.scss';
 
-const Contacts = () => (
+const Contacts = () => {
+  const [message, setMessage] = useState(false)
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    setMessage(true)
+  }
+  return (
   <div className="contact" id="contacts">
     <div className="left">
       <img src="/shake.svg" alt="" />
     </div>
     <div className="right">
       <h2>Contact.</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder='Email' />
         <textarea placeholder='Message' />
         <button type='submit'>Send</button>
       </form>
+      {message && <span>Thanks, I'll reply ASAP :)</span>}
     </div>
   </div>
-);
+)
+};
 
 export default Contacts;
