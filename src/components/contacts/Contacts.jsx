8afec8ef5f957d0633488e73cecd75d9bb60f198
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-17 20:59:41 +0300

    Add error message

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -4,13 +4,14 @@ import './contacts.scss';
 
 const Contacts = () => {
   const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
   const sendEmail = (e) => {
     e.preventDefault();
     emailjs.sendForm('service_js8iquw', 'template_7dhiwmf', e.target, 'czVI_u4x7LdAvviyK')
       .then((result) => {
         setMessage(`Message sent ${result.text}, I will reply as soon as possible.`);
       }, (error) => {
-        // setMessage('Something went wrong, please try again.');
+        setError(error.text);
       });
   };
   return (
@@ -24,7 +25,7 @@ const Contacts = () => {
           <input type="text" placeholder="Email" name="email" />
           <textarea placeholder="Message" name="message" />
           <button type="submit">Send</button>
-          
+      {error && <span className='error'>Something went wrong, please try again.</span> }
       {message && <span>{message}</span> }
         </form>
       </div>
