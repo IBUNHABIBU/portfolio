7e1dff40ac976bc60d6de62e34c7e51efbb738c4
IBUNHABIBU <ibunhabib@gmail.com>
2025-04-10 23:27:17 +0300

    Add images

--- src/constants/index.js
+++ src/constants/index.js
@@ -192,7 +192,7 @@ export const data = [
   {
     id: 'nino',
     title: 'Ninos Kitchen',
-    icon: 'writing.png',
+    // icon: 'writing.png',
     // desc: 'A kitchen website that allows users to order food online. Users can choose a food from the list of available food and order it. Users can also see the list of food they have ordered and the total amount they have to pay.',
     // imTag: 'https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930',
   },
